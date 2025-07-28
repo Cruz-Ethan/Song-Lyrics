5a69f7593f06eb62a0d83f360eb254b837a98eea
Ethan's GitHub <e.cruz1010047@gmail.com>
2025-07-28 15:51:46 -0700

    added audio to lyrics page

--- scripts/lyrics.js
+++ scripts/lyrics.js
@@ -6,6 +6,7 @@ addSearchBarFunctionality();
 const song = getSong();
 renderSongInfo();
 renderLyrics();
+addSongControlsFunctionality();
 
 function renderSongInfo() {
     const songInfoContainer = document.querySelector('.song-info-container');
@@ -33,4 +34,110 @@ function renderLyrics() {
                                                 </div>`,
                                 '');
             
+}
+
+function addSongControlsFunctionality() {
+    const vocals = document.querySelector('.vocals');
+    const instrumental = document.querySelector('.instrumental');
+
+    let vocalsLoaded = false;
+    let instrumentalLoaded = false;
+
+    vocals.addEventListener('loadeddata', () => {
+        vocalsLoaded = true;
+        if(instrumentalLoaded) {
+            renderControls();
+        }
+    });
+
+    instrumental.addEventListener('loadeddata', () => {
+        instrumentalLoaded = true;
+        if(vocalsLoaded) {
+            renderControls();
+        }
+    });
+
+    vocals.src = `${song.path}/song.mp3`;
+    instrumental.src = `${song.path}/instrumental.mp3`;
+
+    function getTime(timeSeconds) {
+        let seconds = Math.round(timeSeconds) % 60;
+        let minutes = Math.round(timeSeconds / 60);
+        if(seconds < 10) {
+            seconds = `0${seconds}`;
+        }
+        return `${minutes}:${seconds}`;
+    }
+
+    function renderControls() {
+        vocals.pause();
+        instrumental.pause();
+        const currentTime = document.querySelector('.current-time');
+        currentTime.innerHTML = getTime(0);
+        document.querySelector('.duration').innerHTML = getTime(vocals.duration);
+
+        document.querySelector('.song-title-2').innerHTML = song.title;
+        const seekBar = document.querySelector('.song-time');
+        seekBar.max = vocals.duration;
+
+        seekBar.addEventListener('click', () => {
+            let shouldPlayAgain = playButtonIcon.classList.contains('fa-pause')
+            if(shouldPlayAgain) {
+                playButtonIcon.classList.add('fa-play');
+                playButtonIcon.classList.remove('fa-pause');
+                vocals.pause();
+                instrumental.pause();
+                clearInterval(intervalId);
+            }
+
+            vocals.currentTime = seekBar.value;
+            instrumental.currentTime = seekBar.value;
+            currentTime.innerHTML = getTime(seekBar.value);
+            const percentage = Math.round(100 * seekBar.value / seekBar.max);
+            seekBar.style.background = `linear-gradient(90deg, var(--highlight) ${percentage}%, var(--text-normal) ${percentage}%)`;
+
+            if(shouldPlayAgain) {
+                playButtonIcon.classList.remove('fa-play');
+                playButtonIcon.classList.add('fa-pause');
+                vocals.play();
+                instrumental.play();
+                intervalId = setInterval(() => {
+                    seekBar.value = vocals.currentTime;
+                    currentTime.innerHTML = getTime(vocals.currentTime);
+                    const percentage = Math.round(100 * seekBar.value / seekBar.max);
+                    seekBar.style.background = `linear-gradient(90deg, var(--highlight) ${percentage}%, var(--text-normal) ${percentage}%)`;
+                }, 500);
+            }
+        });
+
+        const playButton = document.querySelector('.play-button');
+        const playButtonIcon = document.querySelector('.play-button-icon');
+        let intervalId;
+        playButton.addEventListener('click', () => {
+            playButtonIcon.classList.toggle('fa-play');
+            playButtonIcon.classList.toggle('fa-pause');
+
+            if(playButtonIcon.classList.contains('fa-pause')) {
+                vocals.play();
+                instrumental.play();
+                intervalId = setInterval(() => {
+                    seekBar.value = vocals.currentTime;
+                    currentTime.innerHTML = getTime(vocals.currentTime);
+                    const percentage = Math.round(100 * seekBar.value / seekBar.max);
+                    seekBar.style.background = `linear-gradient(90deg, var(--highlight) ${percentage}%, var(--text-normal) ${percentage}%)`;
+                }, 500);
+            }
+            else {
+                vocals.pause();
+                instrumental.pause();
+                clearInterval(intervalId);
+            }
+        });
+
+        const toggleVocalsButton = document.querySelector('.toggle-vocals');
+        toggleVocalsButton.addEventListener('click', () => {
+            vocals.muted = !vocals.muted;
+            instrumental.muted = !instrumental.muted;
+        })
+    }
 }
